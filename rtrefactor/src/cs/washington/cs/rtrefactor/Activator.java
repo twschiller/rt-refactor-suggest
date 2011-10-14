519427a88caf67893200be23a194dd31405f965d
Todd Schiller <tws@cs.washington.edu>
2011-10-14 12:48:11 -0700

    try #1: initial commit of shell Eclipse plugin

---
+++ rtrefactor/src/cs/washington/cs/rtrefactor/Activator.java
@@ -0,0 +1,74 @@
+package edu.washington.cs.rtrefactor;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.IStartup;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin implements IStartup{
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "edu.washington.cs.rtrefactor"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void start(BundleContext context) throws Exception {
+		//this method is called when the plugin is loaded on-demand
+		
+		super.start(context);
+		plugin = this;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void earlyStartup() {
+		//this method is called when Eclipse starts
+		//refer to the documentation for the limitations
+		
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+}
