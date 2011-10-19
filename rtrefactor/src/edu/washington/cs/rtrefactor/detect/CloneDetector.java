81d69c3d01527244bd242cd524aa81b38648dd92
Todd Schiller <twschiller@users.noreply.github.com>
2011-10-19 12:45:55 -0700

    Corrected / improved the usage example for the JCCD clone detector

--- rtrefactor/src/edu/washington/cs/rtrefactor/detect/CloneDetector.java
+++ rtrefactor/src/edu/washington/cs/rtrefactor/detect/CloneDetector.java
@@ -1,6 +1,8 @@
 package edu.washington.cs.rtrefactor.detect;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.EnumSet;
 import java.util.List;
 
@@ -11,15 +13,21 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eposoft.jccd.data.ASourceUnit;
 import org.eposoft.jccd.data.JCCDFile;
 import org.eposoft.jccd.data.SimilarityGroupManager;
 import org.eposoft.jccd.data.SimilarityPair;
+import org.eposoft.jccd.data.SourceUnitPosition;
+import org.eposoft.jccd.data.ast.ANode;
+import org.eposoft.jccd.data.ast.NodeTypes;
 import org.eposoft.jccd.detectors.APipeline;
 import org.eposoft.jccd.detectors.ASTDetector;
 import org.eposoft.jccd.preprocessors.java.*;
 
 import com.google.common.base.Function;
+import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
+import com.google.common.io.Files;
 
 /**
  * A simple clone detector using the <a href="http://jccd.sourceforge.net/">JCCD clone detection pipeline</a>  
@@ -72,12 +80,51 @@ public abstract class CloneDetector {
 	}
 	
 	/**
+	 * Get the {@link File} that the source unit is contained in
+	 * @param node the source unit
+	 * @return the file the source unit is contained in
+	 */
+	public static File getFile(ASourceUnit node){
+		ANode fileNode = (ANode) node;
+		while (fileNode.getType() != NodeTypes.FILE.getType()) {
+			fileNode = fileNode.getParent();
+		}
+		return new File(fileNode.getText());
+	}
+	
+	/**
+	 * Get the source <i>lines</i> associated with {@code node}
+	 * @param node the source unit
+	 * @return the source lines corresponding to the source unit
+	 * @throws IOException iff there is an error reading the file referred to by the source unit
+	 */
+	public static String getSource(ASourceUnit node) throws IOException{
+		List<String> lines = Files.readLines(getFile(node), Charset.defaultCharset());
+
+		SourceUnitPosition minPos = APipeline.getFirstNodePosition((ANode) node);
+		SourceUnitPosition maxPos = APipeline.getLastNodePosition((ANode) node);	
+
+		return Joiner.on(System.getProperty("line.separator")).join(lines.subList(minPos.getLine(), maxPos.getLine()));
+	}
+	
+	/**
+	 * Get the number of lines in a source unit
+	 * @param node the source unit
+	 * @return the number of lines in the source unit
+	 */
+	public static int cloneLength(ASourceUnit node){
+		SourceUnitPosition minPos = APipeline.getFirstNodePosition((ANode) node);
+		SourceUnitPosition maxPos = APipeline.getLastNodePosition((ANode) node);	
+		return maxPos.getLine() - minPos.getLine() + 1;
+	}
+	
+	/**
 	 * Compute the quality score for a clone pair
 	 * @param clonePair the clone pair
 	 * @return the quality score for the clone pair
 	 */
 	public static double qualityScore(SimilarityPair clonePair){
-		return (clonePair.getFirstNode().getText().length() + clonePair.getSecondNode().getText().length()) / 2.0;
+		return (cloneLength(clonePair.getFirstNode()) + cloneLength(clonePair.getSecondNode())) / 2.0;
 	}
 	
 	/**
