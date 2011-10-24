e059a5206783ed8163ae7c0675e172292f81b4bf
Travis.Mandel <Travis.Mandel@localhost>
2011-10-24 16:57:12 -0700

    Merged (+ cleaned-up) hooks to implement a recociler that can highlight text and mark it with a custom marker.

---
+++ rtrefactor/src/edu/washington/cs/rtrefactor/reconciler/CloneViewerConfiguration.java
@@ -0,0 +1,53 @@
+package edu.washington.cs.rtrefactor.reconciler;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.internal.ui.text.JavaCompositeReconcilingStrategy;
+import org.eclipse.jdt.internal.ui.text.JavaReconciler;
+import org.eclipse.jdt.ui.text.IColorManager;
+import org.eclipse.jdt.ui.text.JavaSourceViewerConfiguration;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextDoubleClickStrategy;
+import org.eclipse.jface.text.TextAttribute;
+import org.eclipse.jface.text.presentation.IPresentationReconciler;
+import org.eclipse.jface.text.presentation.PresentationReconciler;
+import org.eclipse.jface.text.reconciler.IReconciler;
+import org.eclipse.jface.text.reconciler.MonoReconciler;
+import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
+import org.eclipse.jface.text.rules.Token;
+import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.jface.text.source.SourceViewerConfiguration;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+public class CloneViewerConfiguration extends JavaSourceViewerConfiguration {
+
+	public CloneViewerConfiguration(IColorManager cm, IPreferenceStore ps, ITextEditor te, String partitioning) {
+		super(cm, ps, te, partitioning);
+		
+		//getQuickAssistAssistant(sourceViewer)
+		//setReconciler 
+	}
+	
+	//For quick fixes: override getquickassistassistant?
+	
+	public IReconciler getReconciler(ISourceViewer sourceViewer) {
+		final ITextEditor editor= getEditor();
+		if (editor != null && editor.isEditable()) {
+
+			//JavaCompositeReconcilingStrategy strategy= new JavaCompositeReconcilingStrategy(sourceViewer, editor, getConfiguredDocumentPartitioning(sourceViewer));
+			
+			CloneReconcilingStrategy cloneStrategy = new CloneReconcilingStrategy(sourceViewer, editor);
+			MonoReconciler reconciler= new MonoReconciler(cloneStrategy, false);
+			reconciler.setIsIncrementalReconciler(false);
+			reconciler.setIsAllowedToModifyDocument(false);
+			reconciler.setProgressMonitor(new NullProgressMonitor());
+			reconciler.setDelay(500);
+
+			return reconciler;
+		}
+		return null;
+	}
+
+	
+
+}
