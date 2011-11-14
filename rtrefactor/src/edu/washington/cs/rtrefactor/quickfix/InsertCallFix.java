abc51bdb73e68eae2f908b1eec6b2177904551f4
Todd Schiller <twschiller@users.noreply.github.com>
2011-11-14 14:04:25 -0800

    Insert method call support

--- rtrefactor/src/edu/washington/cs/rtrefactor/quickfix/InsertCallFix.java
+++ rtrefactor/src/edu/washington/cs/rtrefactor/quickfix/InsertCallFix.java
@@ -3,9 +3,17 @@ package edu.washington.cs.rtrefactor.quickfix;
 import java.io.IOException;
 
 import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
+
+import com.google.common.base.Joiner;
 
 import edu.washington.cs.rtrefactor.detect.SourceRegion;
+import edu.washington.cs.rtrefactor.reconciler.CloneEditor;
 
 /**
  * The quickfix which inserts a call to the second clone's method
@@ -29,21 +37,50 @@ public class InsertCallFix extends CloneFix {
 	
 	@Override
 	public void run(IMarker marker) {
-		MessageDialog.openInformation(null, "Insert Call Demo",
-				"This insert call quick-fix is not yet implemented");
+		
+		IMethod m;
+		try {
+			m = FindMethod.findMethod(this.getOtherRegion());
+		} catch (CoreException e) {
+			MessageDialog.openError(null, "Insert Call", "Error accessing workspace resource: " + e.getMessage());
+			return;
+		}
+		
+	    //http://wiki.eclipse.org/FAQ_How_do_I_insert_text_in_the_active_text_editor%3F
+		IEditorPart editor =  PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+		IDocument doc = ((CloneEditor) editor).getDocumentProvider().getDocument(editor.getEditorInput());
+			
+		int start = this.getSourceRegion().getStart().getGlobalOffset();
+		int len = this.getSourceRegion().getLength();
+		
+		try {
+			doc.replace(start, len, m.getElementName() + "(" + Joiner.on(',').join(m.getParameterNames()) + ")");
+		} catch (Exception e) {
+			MessageDialog.openError(null, "Insert Call", "An error occured when inserting the method call: " + e.getMessage());
+		}
 	}
 
 	@Override
 	public String getDescription() {
-		//TODO: Show method name!
+		IMethod m;
+		String source;
+		String name;
+		try {
+			m = FindMethod.findMethod(this.getOtherRegion());
+			name = m.getElementName();
+			source = m.getSource();
+		} catch (CoreException e) {
+			return "An error occured when locating the method: " + e.getMessage();
+		}
+	
+		// TODO fix this so that spacing and tabs work
+		source = source.replaceAll("[\r\n]+", "<br/>");
+	
 		if(isSameFile()) {
-			return "Replaces code with a call to a method in this file containing" +
-					" the following clone: <br/>" 
-			+ super.getDescription();
+			return "Replaces code with a call to " + name + " in this file:<br/>" + source;
 		} else {
-			return "Replaces code with a call to a method in " + getOtherRegion().getFile().getName() + 
-					" containing the following clone:<br/>" 
-			+ super.getDescription();
+			return "Replaces code with a call to " + name + " in " + getOtherRegion().getFile().getName() + ":<br/>" 
+			+ source;
 		}
 	}
 
