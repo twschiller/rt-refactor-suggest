9078bfd9f50e29fbc8b923153c0c0e0e64650711
Travis.Mandel <Travis.Mandel@localhost>
2011-11-17 19:20:20 -0800

    Tracked down bug, annotations do not disappear on save.  Also removed fancy annotation display.

--- rtrefactor/src/edu/washington/cs/rtrefactor/reconciler/CloneAnnotation.java
+++ rtrefactor/src/edu/washington/cs/rtrefactor/reconciler/CloneAnnotation.java
@@ -2,15 +2,15 @@ package edu.washington.cs.rtrefactor.reconciler;
 
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.ui.texteditor.MarkerAnnotation;
+import org.eclipse.ui.texteditor.SimpleMarkerAnnotation;
 
 /** This trivial class lets us quickly and easily find our own annotations 
  *  in the document. */ 
 
-public class CloneAnnotation extends MarkerAnnotation {
+public class CloneAnnotation extends SimpleMarkerAnnotation {
 	/** The clone annotation type. */
 	public static final String[] CLONE_ANNOTATIONS = 
-		{"rtrefactor.cloneAnnotation","rtrefactor.cloneAnnotation2",
-			"rtrefactor.cloneAnnotation3"} ; 
+		{"rtrefactor.cloneAnnotation"} ; 
 	
 	public CloneAnnotation(IMarker marker, int annotationNumber) {
 		super(CLONE_ANNOTATIONS[annotationNumber % CLONE_ANNOTATIONS.length], marker);
