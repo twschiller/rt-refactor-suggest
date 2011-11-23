87b297f72821792bfb83f157ac264a0fe6eefc6d
Todd.Schiller <Todd.Schiller@localhost>
2011-11-22 21:13:19 -0800

    Include the number of extracted parameters in the scoring for Extract Method

--- rtrefactor/src/edu/washington/cs/rtrefactor/quickfix/FindBlock.java
+++ rtrefactor/src/edu/washington/cs/rtrefactor/quickfix/FindBlock.java
@@ -1,5 +1,6 @@
 package edu.washington.cs.rtrefactor.quickfix;
 
+import java.util.HashSet;
 import java.util.List;
 
 import org.eclipse.core.resources.IProject;
@@ -17,11 +18,24 @@ import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.IBinding;
+import org.eclipse.jdt.core.dom.IVariableBinding;
+import org.eclipse.jdt.core.dom.Name;
+import org.eclipse.jdt.core.dom.QualifiedName;
+import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.Statement;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
+
+import soot.Modifier;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
 import edu.washington.cs.rtrefactor.detect.SourceRegion;
+
+// TODO we need to penalize for occurrences of local variables use a variable that would be 
+// defined locally in the extracted method
 
 /**
  * Methods for finding and scoring the code to extract
@@ -59,6 +73,20 @@ public class FindBlock {
 		public int getEnd(){
 			Statement last =  statements.get(statements.size() - 1);
 			return last.getStartPosition() + last.getLength();
+		}
+		
+		/**
+		 * Get the number of variables that would are captured by the statements
+		 * in this block. Captured variables are those that are used, but not declared within
+		 * the block (excluding static variables).
+		 * @return the number of captured variables
+		 */
+		public int getNumCapturedVariable(){
+			VariableCaptureCounter counter = new VariableCaptureCounter();
+			for (Statement s : statements){
+				s.accept(counter);
+			}
+			return counter.captured.size();
 		}
 	}
 
@@ -114,7 +142,7 @@ public class FindBlock {
 	
 	/**
 	 * Parse the compilation unit using {@link AST#JLS3}, which supports Java 1.5 features.
-	 * Does not perform type resolution.
+	 * Resolved bindings.
 	 * @param unit the Eclipse compilation unit
 	 * @return the compilation unit in AST form
 	 */
@@ -122,7 +150,7 @@ public class FindBlock {
 		ASTParser parser = ASTParser.newParser(AST.JLS3); 
 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
 		parser.setSource(unit); // set source
-		parser.setResolveBindings(false); // we need bindings later on
+		parser.setResolveBindings(true); // we need bindings later on
 		return (CompilationUnit) parser.createAST(null /* IProgressMonitor */); // parse
 	}
 	
@@ -181,4 +209,65 @@ public class FindBlock {
 			return true;
 		}
 	}
+
+	/**
+	 * Collects the set of captured variables, pass in a sequence of statements.
+	 * @author Todd Schiller
+	 */
+	private static class VariableCaptureCounter extends ASTVisitor{
+		
+		/**
+		 * Variable ID of the first seen variable. <code>id < first</code> implies
+		 * that variable with <code>id</code> has been captured.
+		 */
+		private int first = Integer.MAX_VALUE;
+		
+		/**
+		 * Fully qualified names of captured variables
+		 */
+		protected HashSet<String> captured = Sets.newHashSet();
+		
+		@Override
+		public boolean visit(VariableDeclarationStatement x){
+			// only have to look at the first entry because we only care about the lowest number
+			VariableDeclarationFragment f = (VariableDeclarationFragment) x.fragments().get(0);
+			IVariableBinding binding = (IVariableBinding) f.getName().resolveBinding();
+			first = Math.min(first, binding.getVariableId());
+			return true;
+		}
+		
+		@Override
+		public boolean visit(SimpleName name){
+			doName(name);
+			return false;
+		}
+		
+		@Override
+		public boolean visit(QualifiedName name){
+			doName(name);
+			return false;
+		}
+		
+		/**
+		 * Add to set of captured variables if the binding is for a field, or the occurrence
+		 * occurs after the first seen variable. Don't penalize for public static variables. 
+		 * @param name the name of variable
+		 */
+		private void doName(Name name){
+			if (!(name.getParent() instanceof VariableDeclarationFragment)){
+				IBinding binding = name.resolveBinding();
+				if (binding instanceof IVariableBinding){
+					IVariableBinding vb = (IVariableBinding) binding;
+					
+					boolean isStatic = (vb.getModifiers() & Modifier.STATIC) > 0;
+					
+					if ((vb.getVariableId() < first || vb.isField() || vb.isParameter()) && !isStatic){
+						captured.add(name.getFullyQualifiedName());
+					}
+				}
+			}
+
+		}
+	}
+
 }
