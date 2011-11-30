e74ad930b873b599616899ee52b645914d3019ed
Travis.Mandel <tmandel@cs.washington.edu>
2011-11-30 11:18:37 -0800

    Improved copy/paste to do simple variable name replacement.

---
+++ rtrefactor/src/edu/washington/cs/rtrefactor/listeners/DocumentChangedListener.java
@@ -0,0 +1,24 @@
+package edu.washington.cs.rtrefactor.listeners;
+
+import org.eclipse.jface.text.DocumentEvent;
+import org.eclipse.jface.text.IDocumentListener;
+
+/**
+ * Example listener for document change events (e.g., text added / deleted)
+ * @author Todd Schiller
+ */
+public class DocumentChangedListener implements IDocumentListener {
+
+	@Override
+	public void documentAboutToBeChanged(DocumentEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void documentChanged(DocumentEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
