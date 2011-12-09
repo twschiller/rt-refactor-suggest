0e0e5e5bae19183062eb11b54728b16f7d258bb7
Todd Schiller <twschiller@users.noreply.github.com>
2011-12-09 01:08:26 -0800

    Updated public maintenance test

---
+++ eval/QuickPicture/src/edu/washington/cs/rtrefactor/eval/ImageTransform.java
@@ -0,0 +1,13 @@
+package edu.washington.cs.rtrefactor.eval;
+
+
+public interface ImageTransform {
+
+        /**
+         * Transform an image
+         * @param old the original image
+         * @return the new image
+         */
+        public QuickPicture transform(QuickPicture old);
+        
+}
