b6e1bbc0d6fef493cd7552f37da97e392c19e5e7
Todd.Schiller <Todd.Schiller@localhost>
2011-12-09 11:43:13 -0800

    Used Eclipse autoformat on hummingbird for eval; added eval TODOs to prep codebase

--- eval/QuickPicture/src/edu/washington/cs/rtrefactor/eval/PaintShop.java
+++ eval/QuickPicture/src/edu/washington/cs/rtrefactor/eval/PaintShop.java
@@ -18,8 +18,6 @@ import edu.washington.cs.rtrefactor.eval.transform.CartoonifyImageTransform;
 import edu.washington.cs.rtrefactor.eval.transform.CartoonifyImageTransform.CartoonStyle;
 import edu.washington.cs.rtrefactor.eval.transform.CinematicImageTransform;
 import edu.washington.cs.rtrefactor.eval.transform.NewImageTransform;
-import edu.washington.cs.rtrefactor.eval.transform.PhotographicImageTransform;
-import edu.washington.cs.rtrefactor.eval.transform.PhotographicImageTransform.PhotoMode;
 import edu.washington.cs.rtrefactor.eval.transform.RandomSceneGenerator;
 import edu.washington.cs.rtrefactor.eval.transform.UnderwaterTransform;
 import edu.washington.cs.rtrefactor.eval.transform.VisionImageTransform;
@@ -36,109 +34,109 @@ import edu.washington.cs.rtrefactor.eval.util.ImageUtil;
 @SuppressWarnings("serial")
 public class PaintShop extends JFrame{
 
-        private final JPanel transformPanel;
-        private final MainImage image;
-        private final JMenuBar menuBar;
-        private final JMenu fileMenu;
-        
-        private final JFileChooser fc = new JFileChooser();
+	private final JPanel transformPanel;
+	private final MainImage image;
+	private final JMenuBar menuBar;
+	private final JMenu fileMenu;
 
-        
-        private void addTransform(String name, final ImageTransform transform){
-                JButton button = new JButton(name);
-                button.addActionListener(new ActionListener(){
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                                image.update(transform);
-                        }
-                });
-                transformPanel.add(button);
-        }
+	private final JFileChooser fc = new JFileChooser();
 
-        private void addTransforms(){
-                addTransform("Flip", new ImageUtil.Flip());
-                addTransform("Cartoon", new CartoonifyImageTransform(CartoonStyle.EXPERIMENTAL));
+
+	private void addTransform(String name, final ImageTransform transform){
+		JButton button = new JButton(name);
+		button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				image.update(transform);
+			}
+		});
+		transformPanel.add(button);
+	}
+
+	private void addTransforms(){
+		addTransform("Flip", new ImageUtil.Flip());
+		addTransform("Cartoon", new CartoonifyImageTransform(CartoonStyle.EXPERIMENTAL));
 		addTransform("Shrink", new ImageUtil.ShrinkImage(2));
-                addTransform("Vision", new VisionImageTransform(50, 50, 500));
-                addTransform("Underwater", new UnderwaterTransform(false));
-                addTransform("New", new NewImageTransform(17, new QuickColor(-400, 400, -400, 0)));
-                //TODO:Bugs
-                addTransform("Cinematic", new CinematicImageTransform());
-                addTransform("Random", new RandomSceneGenerator(new QuickColor(400,-400,400,0), 100, 100, 200000));
-        }
-        
-        private PaintShop() throws IOException{
-                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                setDefaultLookAndFeelDecorated(true);
-        
-                transformPanel = new JPanel();
-                transformPanel.setLayout(new BoxLayout(transformPanel, BoxLayout.Y_AXIS));
-                addTransforms();
-                
-                menuBar = new JMenuBar();
-                fileMenu = new JMenu("File");
-                menuBar.add(fileMenu);
-                
-                JMenuItem open = new JMenuItem("Open");
-                open.addActionListener(new ActionListener(){
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                                if (fc.showOpenDialog(PaintShop.this) == JFileChooser.APPROVE_OPTION){
-                                        try {
-                                                image.update(QuickPicture.read(fc.getSelectedFile()));
-                                        } catch (IOException ex) {
-                                                System.err.println(ex.getMessage());
-                                                System.exit(1);
-                                        }
-                                }
-                        }
-                });
-                fileMenu.add(open);
-                
-                JMenuItem save = new JMenuItem("Save");
-                save.addActionListener(new ActionListener(){
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                                if (fc.showOpenDialog(PaintShop.this) == JFileChooser.APPROVE_OPTION){
-                                        try {
-                                                image.image().write(fc.getSelectedFile());
-                                        } catch (IOException ex) {
-                                                System.err.println(ex.getMessage());
-                                                System.exit(1);
-                                        }
-                                }
-                        }
-                });
-                fileMenu.add(save);
-                
-                JMenuItem exit = new JMenuItem("Exit");
-                exit.addActionListener(new ActionListener(){
-                        @Override
-                        public void actionPerformed(ActionEvent arg0) {
-                                System.exit(0);
-                        }
-                });
-                fileMenu.add(exit);
-                
-                image = new MainImage(400,400);
-                
-                getContentPane().setLayout(new BorderLayout());
-                        
-                getContentPane().add(transformPanel, BorderLayout.LINE_START);
-                getContentPane().add(image, BorderLayout.CENTER);
-                
-                this.setJMenuBar(menuBar);
-                setVisible(true);
-                
-                this.setSize(500, 400);
-        }
-        
-        /**
-         * @param args
-         * @throws IOException 
-         */
-        public static void main(String[] args) throws IOException {
-                new PaintShop();
-        }
+		addTransform("Vision", new VisionImageTransform(50, 50, 500));
+		addTransform("Underwater", new UnderwaterTransform(false));
+		addTransform("New", new NewImageTransform(17, new QuickColor(-400, 400, -400, 0)));
+		//TODO:Bugs
+		addTransform("Cinematic", new CinematicImageTransform());
+		addTransform("Random", new RandomSceneGenerator(new QuickColor(400,-400,400,0), 100, 100, 200000));
+	}
+
+	private PaintShop() throws IOException{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultLookAndFeelDecorated(true);
+
+		transformPanel = new JPanel();
+		transformPanel.setLayout(new BoxLayout(transformPanel, BoxLayout.Y_AXIS));
+		addTransforms();
+
+		menuBar = new JMenuBar();
+		fileMenu = new JMenu("File");
+		menuBar.add(fileMenu);
+
+		JMenuItem open = new JMenuItem("Open");
+		open.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (fc.showOpenDialog(PaintShop.this) == JFileChooser.APPROVE_OPTION){
+					try {
+						image.update(QuickPicture.read(fc.getSelectedFile()));
+					} catch (IOException ex) {
+						System.err.println(ex.getMessage());
+						System.exit(1);
+					}
+				}
+			}
+		});
+		fileMenu.add(open);
+
+		JMenuItem save = new JMenuItem("Save");
+		save.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (fc.showOpenDialog(PaintShop.this) == JFileChooser.APPROVE_OPTION){
+					try {
+						image.image().write(fc.getSelectedFile());
+					} catch (IOException ex) {
+						System.err.println(ex.getMessage());
+						System.exit(1);
+					}
+				}
+			}
+		});
+		fileMenu.add(save);
+
+		JMenuItem exit = new JMenuItem("Exit");
+		exit.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		fileMenu.add(exit);
+
+		image = new MainImage(400,400);
+
+		getContentPane().setLayout(new BorderLayout());
+
+		getContentPane().add(transformPanel, BorderLayout.LINE_START);
+		getContentPane().add(image, BorderLayout.CENTER);
+
+		this.setJMenuBar(menuBar);
+		setVisible(true);
+
+		this.setSize(500, 400);
+	}
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		new PaintShop();
+	}
 
 }
--- eval/QuickPicture/src/edu/washington/cs/rtrefactor/eval/transform/CinematicImageTransform.java
+++ eval/QuickPicture/src/edu/washington/cs/rtrefactor/eval/transform/CinematicImageTransform.java
@@ -12,77 +12,77 @@ import edu.washington.cs.rtrefactor.eval.QuickPicture;
  */
 public strictfp class CinematicImageTransform implements ImageTransform {
 
-        
-        /**
-         * Transforms the image to make it more cinematic:
-         * 
-         * This is a two-phase process, with the first phase changing the focus,
-         * and the second phase swapping out colors to give it that old-timey video feel.
-         */
-        @Override
-        public QuickPicture transform(QuickPicture old) {
-                //odd, square same size kernels only
-                double[]  kernel = {0.05, 0.1, 0.05,
-                                                        0.1, 0.4, 0.1,
-                                                        0.05, 0.1, 0.05};
-                
-                double[]  kernel2 = {0, 0.5, 0,
-                                                        0, -1, 0,
-                                                        0, 0.5, 0};
-                
-                QuickPicture result = new QuickPicture(old.getWidth(), old.getHeight());
-                
-                int len = (int)Math.sqrt(kernel2.length);
-                int sideoff =  (len-1)/2;
-                
-                for (int x = 0 ; x < old.getHeight(); x++){
-                        for (int y = 0; y < old.getWidth() ; y ++){
-                                double totalColor=0;
-                                for (int xo = Math.max(x-sideoff, 0); xo <= Math.min(x+sideoff, old.getHeight()-1); xo++){
-                                        for (int yo = Math.max(y-sideoff, 0); yo <= Math.min(y+sideoff, old.getHeight()-1); yo++){
-                                                QuickColor color = old.getColor(xo, yo);
-                                                int kernelr = xo -(x-sideoff);
-                                                int kernelc = yo - (y-sideoff);
-                                                double kFactor = kernel2[kernelr*len + kernelc];
-                                                totalColor += kFactor *color.getRed();
-                                                totalColor += kFactor *color.getGreen();
-                                                totalColor += kFactor *color.getBlue();
-                                        }
-                                }
-                                
-                                if(totalColor > 0){
-                                        result.setColor(x,y, old.getColor(x, y));
-                                } else {
-                                        result.setColor(x, y, new QuickColor(0,0,0, old.getColor(x, y).getAlpha()));
-                                }
-                                        
-                
-                        }
-                }
-                
-                
-                for (int x = 0 ; x < result.getHeight(); x++){
-                        for (int y = 0; y < result.getWidth() ; y ++){
-                                double totalRed =0, totalGreen=0, totalBlue=0;
-                                for (int xo = Math.max(x-sideoff, 0); xo <= Math.min(x+sideoff, result.getHeight()-1); xo++){
-                                        for (int yo = Math.max(y-sideoff, 0); yo <= Math.min(y+sideoff, result.getHeight()-1); yo++){
-                                                QuickColor color = result.getColor(xo, yo);
-                                                int kernelr = xo -(x-sideoff);
-                                                int kernelc = yo - (y-sideoff);
-                                                double kFactor = kernel[kernelr*len + kernelc];
-                                                totalRed += kFactor *color.getRed();
-                                                totalGreen += kFactor *color.getGreen();
-                                                totalBlue += kFactor *color.getBlue();
-                                        }
-                                }
-                                result.setColor(x, y, new QuickColor((int)totalRed, (int)totalGreen, (int)totalBlue, 
-                                                result.getColor(x, y).getAlpha()));
-                
-                        }
-                }
-                return result;
-        }
 
-        
-        
+	/**
+	 * Transforms the image to make it more cinematic:
+	 * 
+	 * This is a two-phase process, with the first phase changing the focus,
+	 * and the second phase swapping out colors to give it that old-timey video feel.
+	 */
+	@Override
+	public QuickPicture transform(QuickPicture old) {
+		//odd, square same size kernels only
+		double[]  kernel = {0.05, 0.1, 0.05,
+				0.1, 0.4, 0.1,
+				0.05, 0.1, 0.05};
+
+		double[]  kernel2 = {0, 0.5, 0,
+				0, -1, 0,
+				0, 0.5, 0};
+
+		QuickPicture result = new QuickPicture(old.getWidth(), old.getHeight());
+
+		int len = (int)Math.sqrt(kernel2.length);
+		int sideoff =  (len-1)/2;
+
+		for (int x = 0 ; x < old.getHeight(); x++){
+			for (int y = 0; y < old.getWidth() ; y ++){
+				double totalColor=0;
+				for (int xo = Math.max(x-sideoff, 0); xo <= Math.min(x+sideoff, old.getHeight()-1); xo++){
+					for (int yo = Math.max(y-sideoff, 0); yo <= Math.min(y+sideoff, old.getHeight()-1); yo++){
+						QuickColor color = old.getColor(xo, yo);
+						int kernelr = xo -(x-sideoff);
+						int kernelc = yo - (y-sideoff);
+						double kFactor = kernel2[kernelr*len + kernelc];
+						totalColor += kFactor *color.getRed();
+						totalColor += kFactor *color.getGreen();
+						totalColor += kFactor *color.getBlue();
+					}
+				}
+
+				if(totalColor > 0){
+					result.setColor(x,y, old.getColor(x, y));
+				} else {
+					result.setColor(x, y, new QuickColor(0,0,0, old.getColor(x, y).getAlpha()));
+				}
+
+
+			}
+		}
+
+
+		for (int x = 0 ; x < result.getHeight(); x++){
+			for (int y = 0; y < result.getWidth() ; y ++){
+				double totalRed =0, totalGreen=0, totalBlue=0;
+				for (int xo = Math.max(x-sideoff, 0); xo <= Math.min(x+sideoff, result.getHeight()-1); xo++){
+					for (int yo = Math.max(y-sideoff, 0); yo <= Math.min(y+sideoff, result.getHeight()-1); yo++){
+						QuickColor color = result.getColor(xo, yo);
+						int kernelr = xo -(x-sideoff);
+						int kernelc = yo - (y-sideoff);
+						double kFactor = kernel[kernelr*len + kernelc];
+						totalRed += kFactor *color.getRed();
+						totalGreen += kFactor *color.getGreen();
+						totalBlue += kFactor *color.getBlue();
+					}
+				}
+				result.setColor(x, y, new QuickColor((int)totalRed, (int)totalGreen, (int)totalBlue, 
+						result.getColor(x, y).getAlpha()));
+
+			}
+		}
+		return result;
+	}
+
+
+
 }
