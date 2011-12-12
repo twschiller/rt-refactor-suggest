58019862ef73003e8be20e93420c4e5b60037f69
Todd.Schiller <Todd.Schiller@localhost>
2011-12-12 14:04:45 -0800

    GenerateTargets now produced PNG extension; updated images for hummingbird

--- eval/QuickPicture/src/edu/washington/cs/rtrefactor/eval/tests/GenerateTargets.java
+++ eval/QuickPicture/src/edu/washington/cs/rtrefactor/eval/tests/GenerateTargets.java
@@ -36,18 +36,18 @@ public class GenerateTargets {
         puppy = QuickPicture.read(new File(Common.IMAGE_DIR, "Puppy.jpg"));
         ghost = QuickPicture.read(new File(Common.IMAGE_DIR, "Ghost.png"));
 
-        writeTarget(astronaut, new NewImageTransform(17, new QuickColor(-400, 400, -400, 0)), "Astronaut_new.jpg");
-        writeTarget(puppy, new NewImageTransform(10, new QuickColor(-400, 400, 400, 0)), "Puppy_new.jpg");
+        writeTarget(astronaut, new NewImageTransform(17, new QuickColor(-400, 400, -400, 0)), "Astronaut_new.png");
+        writeTarget(puppy, new NewImageTransform(10, new QuickColor(-400, 400, 400, 0)), "Puppy_new.png");
 
         writeTarget(ghost, new ImageUtil.ShrinkImage(2), "Ghost_shrink.png");
 
-        writeTarget(puppy, new UnderwaterTransform(false), "Puppy_underwater.jpg");            
-        writeTarget(astronaut, new UnderwaterTransform(false), "Astronaut_underwater.jpg");
+        writeTarget(puppy, new UnderwaterTransform(false), "Puppy_underwater.png");            
+        writeTarget(astronaut, new UnderwaterTransform(false), "Astronaut_underwater.png");
 
         writeTarget(astronaut, new ImageUtil.Flip(), "Astronaut_flip.jpg");
-        writeTarget(puppy,  new CartoonifyImageTransform(CartoonStyle.EXPERIMENTAL), "Puppy_cartoon.jpg");
-        writeTarget(astronaut, new VisionImageTransform(50, 50, 500), "Astronaut_vision.jpg");
-        writeTarget(puppy,  new CinematicImageTransform(), "Puppy_movie.jpg");
+        writeTarget(puppy,  new CartoonifyImageTransform(CartoonStyle.EXPERIMENTAL), "Puppy_cartoon.png");
+        writeTarget(astronaut, new VisionImageTransform(50, 50, 500), "Astronaut_vision.png");
+        writeTarget(puppy,  new CinematicImageTransform(), "Puppy_movie.png");
 
         // HIDDEN TESTS
 
